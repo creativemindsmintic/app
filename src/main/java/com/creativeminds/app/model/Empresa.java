71b21882cebdaab5dbfdf74c549ec266518478c1
johnsito <jotazo@hotmail.com>
2022-08-24 13:16:06 +0000

    MVC y primera clase de modelo

---
+++ src/main/java/com/creativeminds/app/model/Empresa.java
@@ -0,0 +1,58 @@
+package com.creativeminds.app.model;
+
+public class Empresa {
+    int id;
+    String Nombre;
+    String Direccion;
+    String telefono;
+    String nit;
+    Boolean activo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}
