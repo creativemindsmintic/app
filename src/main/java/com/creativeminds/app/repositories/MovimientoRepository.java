3e0416139879b6c3171ebdb8d21ead1a2ad3173b
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-08-31 20:07:05 -0500

    Actualización

--- src/main/java/com/creativeminds/app/repositories/MovimientoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.creativeminds.app.repositories;
-
-import com.creativeminds.app.model.MovimientoDinero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovimientoRepository extends JpaRepository<MovimientoDinero, Integer> {
-}
