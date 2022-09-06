f28b760b2e5d814b1704592f747e346f0ef9f507
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-09-06 14:36:40 -0500

    Corrección en Query de movimientos por empleado y por empresa

---
+++ src/main/java/com/creativeminds/app/controller/MovimientosController.java
@@ -0,0 +1,68 @@
+package com.creativeminds.app.controller;
+
+
+import com.creativeminds.app.model.Empleado;
+import com.creativeminds.app.model.Empresa;
+import com.creativeminds.app.model.MovimientoDinero;
+import com.creativeminds.app.services.EmpleadoService;
+import com.creativeminds.app.services.MovimientosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MovimientosController {
+    @Autowired
+    MovimientosService movimientosService;
+
+    @GetMapping ("/movements")
+    public List<MovimientoDinero> verMovimientos(){
+        List<MovimientoDinero> list = new ArrayList<>();
+        movimientosService.getAllMovimientos().forEach(movimiento -> list.add(movimiento));
+        return list;
+    }
+
+    @PostMapping ("/movements")
+    public MovimientoDinero guardarMovimiento (@RequestBody MovimientoDinero mov){
+        return movimientosService.saveOrUpdateMovimiento(mov);
+    }
+
+    @GetMapping("/movements/{id}")
+    public MovimientoDinero movimientoDineroporID (@PathVariable("id") Integer id){
+        return movimientosService.getMovimientoById(id);
+    }
+
+    @PatchMapping("/movements/{id}")
+    public MovimientoDinero actualizarMovimiento (@PathVariable("id") Integer id, @RequestBody MovimientoDinero movimientoDinero){
+        MovimientoDinero mov = movimientosService.getMovimientoById(id);
+        mov.setEmpleado(movimientoDinero.getEmpleado());
+        mov.setTipo_movi(movimientoDinero.getTipo_movi());
+        mov.setConcepto(movimientoDinero.getConcepto());
+        mov.setFecha(movimientoDinero.getFecha());
+        mov.setMonto(movimientoDinero.getMonto());
+        return movimientosService.saveOrUpdateMovimiento(mov);
+    }
+
+    @DeleteMapping ("/movements/{id}") //Eliminar registro de la bd
+    public String DeleteMovimientoDinero(@PathVariable("id") Integer id){
+        boolean respuesta=movimientosService.deleteMovimiento(id);
+        if (respuesta){  //Si respuesta es true?
+            return "Se elimino el movimiento con id" +id;
+        }else{
+            return "No se pudo eliminar el movimiento con id"+id;
+        }
+    }
+
+    @GetMapping("/users/{id}/movements") //Consultar movimientos por id del empleado
+    public ArrayList<MovimientoDinero> movimientosPorEmpleado(@PathVariable("id") Integer id){
+        return movimientosService.obtenerPorEmpleado(id);
+    }
+
+    @GetMapping("/enterprises/{id}/movements") //Consultar movimientos que pertenecen a una empresa por el id de la empresa
+    public ArrayList<MovimientoDinero> movimientosPorEmpresa(@PathVariable("id") Integer id){
+        return movimientosService.obtenerPorEmpresa(id);
+    }
+
+}
--- src/main/java/com/creativeminds/app/repositories/MovimientoRepository.java
+++ src/main/java/com/creativeminds/app/repositories/MovimientoRepository.java
@@ -2,8 +2,18 @@ package com.creativeminds.app.repositories;
 
 import com.creativeminds.app.model.MovimientoDinero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface MovimientoRepository extends JpaRepository<MovimientoDinero, Integer> {
+    //Metodo para filtrar movimientos por empleado
+    @Query(value ="select * from movimiento_dinero where empleado_id= ?1", nativeQuery = true)
+    public abstract ArrayList<MovimientoDinero> findByEmpleado(Integer id);
+
+    //Metodo para filtrar movimientos por empresa
+    @Query(value="select * from movimiento_dinero where empleado_id in (select id from empleado where empresa_id= ?1)", nativeQuery = true)
+    public abstract ArrayList<MovimientoDinero> findByEmpresa(Integer id);
 }
