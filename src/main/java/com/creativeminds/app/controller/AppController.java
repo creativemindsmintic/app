2577bfbd6172fab249f86dc00264223960e2f235
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-09-20 16:44:18 -0500

    Configuración de Auth0

--- src/main/java/com/creativeminds/app/controller/AppController.java
+++ src/main/java/com/creativeminds/app/controller/AppController.java
@@ -8,6 +8,8 @@ import com.creativeminds.app.services.EmpleadoService;
 import com.creativeminds.app.services.EmpresaService;
 import com.creativeminds.app.services.MovimientosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,14 +20,14 @@ import java.util.List;
 @Controller
 public class AppController {
 
-    //    @GetMapping("/")
-//    public String index() {
-//        return "index";
-//    }
-    @GetMapping("/")
-    public String plantilla() {
-        return "Plantilla";
+        @GetMapping("/")
+    public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
     }
+//    @GetMapping("/")
+//    public String plantilla() {
+//        return "Plantilla";
+//    }
 
     @GetMapping("/inicio")
     public String Inicio() {
