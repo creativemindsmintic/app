43e42935d1045782a89fadc2b39459bc61b3e4e3
johnsito <jotazo@hotmail.com>
2022-09-23 15:19:51 -0500

    logout Oauth

--- src/main/java/com/creativeminds/app/repositories/UserRepository.java
+++ src/main/java/com/creativeminds/app/repositories/UserRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
-    User findByAuth0Id(String aoth0Id);
-    User findByImage(String Image);
 
 }
